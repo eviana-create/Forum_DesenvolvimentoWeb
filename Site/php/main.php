b7110362e61d3d20ef47a6cd0df31123d729c330
eviana-create <missonzito@outlook.com>
2022-10-14 18:44:48 -0300

    Desenvolvimento do back-end da pagina de signos

---
+++ Site/php/main.php
@@ -0,0 +1,102 @@
+<?php
+$data_nascimento = $_POST["Data de Nascimento"];
+$signos = $_POST = $_POST["Virgem, Libra, Escorpião, Sagitario, Capricornio, Aquario, Peixes"];
+
+echo "Óla Mundo !";
+    ?>
+
+<?xml version="1.0"?>
+
+<signos>
+
+<signo>
+
+<dataInicio>21/03</dataInicio>
+
+<dataFim>20/04</dataFim>
+
+<signoNome>Áries</signoNome>
+
+<descricao>Lorem ipsum dolor sit amet.</descricao>
+
+</signo>
+
+<signo>
+
+<dataInicio>21/04</dataInicio>
+
+<dataFim>20/05</dataFim>
+
+<signoNome>Touro</signoNome>
+
+<descricao>Lorem ipsum dolor sit amet.</descricao>
+
+</signo>
+
+<signo>
+
+<dataInicio>21/03</dataInicio>
+
+<dataFim>20/04</dataFim>
+
+<signoNome>Aquario</signoNome>
+
+<descricao>Lorem ipsum dolor sit amet.</descricao>
+
+</signo>
+
+<signoNome>Capricornio</signoNome>
+
+<descricao>Lorem ipsum dolor sit amet.</descricao>
+
+</signo>
+
+<signoNome>Libra</signoNome>
+
+<descricao>Lorem ipsum dolor sit amet.</descricao>
+
+</signo>
+
+<signoNome>Cancer</signoNome>
+
+<descricao>Lorem ipsum dolor sit amet.</descricao>
+
+</signo>
+
+<signoNome>Escorpião</signoNome>
+
+<descricao>Lorem ipsum dolor sit amet.</descricao>
+
+</signo>
+
+<signoNome>Gemeos</signoNome>
+
+<descricao>Lorem ipsum dolor sit amet.</descricao>
+
+</signo>
+
+<signoNome>Leão</signoNome>
+
+<descricao>Lorem ipsum dolor sit amet.</descricao>
+
+</signo>
+
+<signoNome>Peixes</signoNome>
+
+<descricao>Lorem ipsum dolor sit amet.</descricao>
+
+</signo>
+
+<signoNome>Peixes</signoNome>
+
+<descricao>Lorem ipsum dolor sit amet.</descricao>
+
+</signo>
+
+<signoNome>Sagitario</signoNome>
+
+<descricao>Lorem ipsum dolor sit amet.</descricao>
+
+</signo>
+
+</signos>
